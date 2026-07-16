a6ccf1f7fa848c09b17c86bffeca2a200b84619a
박성진 <macboy0503@naver.com>
2026-07-16 13:31:47 +0900

    refactor : cors

--- 00.StartCamp_6team_project/backend/app/main.py
+++ 00.StartCamp_6team_project/backend/app/main.py
@@ -18,6 +18,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://ssafystartcamp-git-main-macboy5s-projects.vercel.app",
+        "https://ssafystartcamp.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
